e98e6a2e1e324500fb736118e41039c2b1735e65
cortabear <cortabear@gmail.com>
2023-10-29 15:54:52 -0500

    Updated

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Elohim
 Flagship wordpress theme developed by the good developers at the mighty Adbyrd Advertising Agency.  
-  
+
+# Technologies  
+
+## TailwindCSS  
+Visit [TailwindCSS](https://tailwindcss.com)
+
+
 --- 
 
 Visit [Adbyrd](https://adbyrd.com) for more information.
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * Main Template File
+ * @package Elohim
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Elohim</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <h1>Elohim</h1>
+    </body>
+</html>
